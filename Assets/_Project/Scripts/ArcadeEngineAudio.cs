1f0e7f4a02c266bce45e3ca9f1a9e6f8d81bb8d1
GQBrendel <guilherme.brendel@gmail.com>
2022-06-25 12:02:35 -0300

    Includes 2D Platform Sample.

---
+++ Assets/_Project/Scripts/ArcadeEngineAudio.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace KartGame.KartSystems
+{
+    /// <summary>
+    /// This class produces audio for various states of the vehicle's movement.
+    /// </summary>
+    public class ArcadeEngineAudio : MonoBehaviour
+    {
+        public float minRPM = 0;
+        public float maxRPM = 5000;
+        public float kartSpeed = 100f;
+
+        void Awake()
+        {
+        }
+
+        void Update()
+        {
+            //float kartSpeed = arcadeKart != null ? arcadeKart.LocalSpeed() : 0;
+            // set RPM value for the FMOD event
+            float effectiveRPM = Mathf.Lerp(minRPM, maxRPM, kartSpeed);
+            var emitter = GetComponent<FMODUnity.StudioEventEmitter>();
+            emitter.SetParameter("RPM", effectiveRPM);
+        }
+    }
+}
